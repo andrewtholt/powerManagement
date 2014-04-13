1be4ad81248be4bd44218d81756af4c50a1074e1
Andrew Holt <andrewtholt60@gmail.com>
2014-04-13 18:29:35 +0100

    Changed verbose messages.

--- PDU/check.py
+++ PDU/check.py
@@ -8,12 +8,18 @@
 import sys
 import getopt
 from pysqlite2 import dbapi2 as sqlite
+from os import getenv
 
 import os.path
 from os import getenv,popen,remove
 from time import sleep
 
 def main():
+
+    verbose=False
+    if len(sys.argv) > 1:
+        verbose=True
+
     db = getenv("POWER_DB")
     if db == None:
         db = "/etc/local/power/data/power.db"
@@ -36,7 +42,7 @@ def main():
         from hosts,outlets where hosts.idx=outlets.hostidx;"""
     cur.execute(sql)
     for r in cur.fetchall():
-        print r[0],r[1],r[2],r[3]
+#        print r[0],r[1],r[2],r[3]
         
         pduName=r[0]
         outletName =r[1]
@@ -49,11 +55,11 @@ def main():
         rw=r[7]
         hostStatus = r[8]
 
-        print "Host %s Host status %s" % ( pduName, hostStatus )
         
         if hostStatus != "DOWN":
             cmd = "snmpget -OvQ -t 10 -v1 -c %s %s %s 2> /dev/null" % ( ro,pduName,oid)
-            print cmd
+            if verbose:
+                print cmd
         
             t = popen( cmd )
             ret =  int( t.readline() )
@@ -64,9 +70,13 @@ def main():
             elif ret == int(offValue):
                 cState="OFF"
 
+            print "Host %s Host status %s" % ( cState, outletName )
             sqlCmd = "update outlets set state='%s' where name='%s';" % ( cState, outletName )
-#        print sqlCmd
-#        print "===================================================="
+
+            if verbose:
+                print sqlCmd
+                print "===================================================="
+
             cur.execute(sqlCmd)
 
 #        if ret == int(onValue):
