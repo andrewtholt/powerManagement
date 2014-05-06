caf2805084bb656b96e929e6577cef350eddd85e
Andrew Holt <andrewtholt60@gmail.com>
2014-05-06 15:13:37 +0100

    Removed set defaults when env unset.

--- PDU/onPowerRestore.py
+++ PDU/onPowerRestore.py
@@ -13,21 +13,18 @@ from os import getenv,path,remove
 def main():
     
     db = getenv("POWER_DB")
-    if db == None:
-        db = "/etc/local/power/data/power.db"
 
     pdir = getenv("PDIR")
-    if pdir == None:
-        pdir = "/usr/local/apps/power"
 
     con = sqlite.connect( db )
     cur = con.cursor()
 
     if path.exists( '/var/tmp/powerfail.txt'):
-        remove('/var/tmp/powerfail.txt')
-        sql="update outlets set pf_state='UNKNOWN';"
-        cur.execute( sql )
-        con.commit()
+        print "Power Failed."
+#        remove('/var/tmp/powerfail.txt')
+#        sql="update outlets set pf_state='UNKNOWN';"
+#        cur.execute( sql )
+#        con.commit()
 
 
     sql = "update outlets set requested_state=pf_action,touched=datetime(\'NOW\') where pf_action <> 'NONE';"
