15143de7e9f075c6c680ebbe5ab9cb01a2408124
Andrew Holt (at portabob) <andrewtholt@gmail.com>
2015-08-19 14:28:07 +0100

    Run this on the machine that hosts teh config files and scripts.

---
+++ NODE/Scripts/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+
+echo $EUID
+
+if [ $EUID -ne 0 ]; then 
+    echo "Must be root, sorry."
+    exit 1
+fi
+
+BASE=/srv/ftp/manage
+
+if [ ! -d $BASE ]; then
+    mkdir -p $BASE
+    mkdir -p $BASE/Data
+    mkdir -p $BASE/Scripts
+fi
+
+cp ../Data/*.ini $BASE/Data
+
+SCRIPTS="debianZram.sh inifile.lua node.lua"
+
+for N in $SCRIPTS; do
+    cp $N $BASE/Scripts
+done
