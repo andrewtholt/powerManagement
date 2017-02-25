f3601d8474505ccf1dbee8e59263af1508fcbd3a
Andrew Holt <andrewtholt60@gmail.com>
2017-02-25 17:28:09 +0000

    Added command to use MQTT to control snmp PDU

---
+++ PDU/dAct.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import paho.mqtt.client as mqtt
+import sqlite3 as sqlite
+from os import getenv,system
+import sys
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("/home/office/+/power")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+    db=getenv("POWER_DB")
+
+    if db == None:
+        print("Environment not setup.")
+        sys.exit(1)
+
+    path=msg.topic.split("/")
+    act=(str(msg.payload)).split("'")[1]
+
+    outlet=path[3]
+    print( outlet )
+    print( act)
+
+
+    print(db)
+
+    sql="select hosts.name,hosts.on_value,hosts.off_value,outlets.name,outlets.oid from hosts,outlets where hosts.name='apc' and outlets.name='%s';" % ( outlet)
+
+    con=sqlite.connect(db)
+    cur=con.cursor()
+
+    cur.execute(sql)
+
+    data=cur.fetchone()
+
+    pdu=data[0]
+
+    onValue=data[1]
+    offValue=data[2]
+    name=data[3]
+    oid=data[4]
+
+    rw='private'
+
+    state=-1
+    if act=='ON':
+        state=onValue
+    elif act=='OFF':
+        state=offValue
+
+    cmd = "snmpset -t 10 -v1 -c %s %s %s i %d > /dev/null 2>&1" % (rw,pdu,oid,state)
+    print(cmd)
+    system(cmd)
+
+    con.close
+
+
+def main():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect("127.0.0.1", 1883, 60)
+
+#    db=getenv("POWER_DB")
+#    con=sqlite.connect(db)
+#    cur=con.cursor()
+#
+#    sql = "select outlets.name from hosts,outlets where hosts.name='apc' and hosts.idx=outlets.hostidx;"
+#    cur.execute(sql)
+#
+#    for r in cur.fetchall():
+#        outletName=r[0]
+#
+#        topic = "/home/office/%s/power" % (outletName)
+#
+#        client.subscribe( topic )
+
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+    client.loop_forever()
+
+
+main()
+
---
+++ PDU/dUpdate.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+#
+# POWER_DB  Database
+# PDIR      Base directory for utilities
+#
+# Read each defined port and set the actual state in the database.
+#
+import sys
+import getopt
+import sqlite3 as sqlite
+
+# from pysqlite2 import dbapi2 as sqlite
+from os import getenv
+
+import os.path
+from os import getenv,popen,remove
+from time import sleep
+
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata,flags,rc):
+    print("Connected with result code "+str(rc))
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+def executeSql(cur,sql):
+    failed = True
+    while failed:
+        try:
+            cur.execute( sql )
+            failed=False
+        except sqlite.OperationalError:
+            print("Database Locked.")
+            failed=True
+            sleep(1)
+    return
+
+def main():
+
+    verbose=False
+    if len(sys.argv) > 1:
+        verbose=True
+
+    db = getenv("POWER_DB")
+
+    pdir = getenv("PDIR")
+
+    if db == None or pdir == None:
+        print ("FATAL ERROR: setup PDIR & POWER_DB env variables")
+        sys.exit(1)
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("127.0.0.1", 1883, 60)
+    print(client)
+    client.loop()
+
+    con = sqlite.connect( db )
+    cur = con.cursor()
+    
+    sql = """select 
+        hosts.name,outlets.name,outlets.state,hosts.ro_community,outlets.oid,
+        hosts.on_value,hosts.off_value,hosts.rw_community,hosts.status,
+        hosts.ip
+        from hosts,outlets where hosts.idx=outlets.hostidx and hosts.status='UP';"""
+
+    if verbose:
+        print (sql)
+
+#    cur.execute(sql)
+    executeSql(cur,sql)
+    for r in cur.fetchall():
+#        print (r[0],r[1],r[2],r[3])
+        
+        pduName=r[0]
+        outletName =r[1]
+        outletState = r[2]
+        ro=r[3]
+        oid=r[4]
+        
+        onValue=r[5]
+        offValue = r[6]
+        rw=r[7]
+        hostStatus = r[8]
+        ip=r[9]
+        
+        if hostStatus != "DOWN":
+            cmd = "snmpget -OvQ -t 10 -v1 -c %s %s %s 2> /dev/null" % ( ro, ip,oid)
+            if verbose:
+                print (cmd)
+        
+            t = popen( cmd )
+            ret =  int( t.readline() )
+        
+            cState="UNKNOWN"
+            if ret == int(onValue):
+                cState="ON"
+            elif ret == int(offValue):
+                cState="OFF"
+
+#            print ("Ret value ",ret)
+#            print ("On Value  ",int(onValue))
+#            print ("Off Value ",int(offValue))
+
+            print ("Host %s Host status %s" % ( outletName,cState ))
+
+            topic="/home/office/%s/power" % ( outletName )
+
+            payload=cState
+
+            print(topic)
+            print(payload)
+
+            client.publish(topic, payload, qos=0, retain=True)
+
+#            sqlCmd = "update outlets set state='%s' where name='%s';" % ( cState, outletName )
+
+#            if verbose:
+#                print (sqlCmd)
+#                print ("====================================================")
+#
+#            executeSql(cur,sql)
+#            cur.execute(sqlCmd)
+
+#            sqlCmd = "update outlets set pf_state='%s' where name='%s' and start_state='RESTORE';" % ( cState, outletName )
+#
+#            if verbose:
+#                print (sqlCmd)
+#                print ("====================================================")
+#
+#            executeSql(cur,sql)
+#            cur.execute(sqlCmd)
+            
+#    con.commit()
+    con.close()
+
+
+
+main()
+
+
+
+
+
+
