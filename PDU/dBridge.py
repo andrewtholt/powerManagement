f4138692f169274aff8ce05f5f082a4121a918f2
Andrew Holt <andrewtholt60@gmail.com>
2017-02-28 21:20:51 +0000

    Started on bridge.

---
+++ PDU/dBridge.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import configparser as cp
+import getopt
+import sys
+import redis
+
+def usage():
+    print()
+    print("Usage:./dBridge.py -h|-c <cfg> -l|-r -v ")
+
+    print("\t-c <cfg>\tConfig file.")
+    print("\t-h\t\tHelp.")
+    print("\t-l\t\tLocal.")
+    print("\t-r\t\tRemote.")
+    print()
+
+def main():
+
+    localBroker  = True
+    remoteBroker = False
+    verbose = False
+    configFile="/etc/mqtt/bridge.ini"
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "c:hlrv", ["config=","help","local","remote","verbose"])
+
+        for o,a in opts:
+            if o in ["-h","--help"]:
+                usage()
+                sys.exit()
+            elif o in ["-l","--local"]:
+                localBroker=True
+                remoteBroker=False
+            elif o in ["-r","--remote"]:
+                localBroker=False
+                remoteBroker=True
+            elif o in ["-v","--verbose"]:
+                verbose=True
+            elif o in ["-c","--config"]:
+                configFile = a
+
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+
+    if verbose:
+        print()
+        if localBroker:
+            print("Local broker")
+        if remoteBroker:
+            print("Remote broker")
+
+        print("Config File:"+configFile)
+        print()
+
+    cfg = cp.ConfigParser()
+    cfg.read( configFile )
+
+    localBrokerHost=""
+    localBrokerPort = 6379
+
+    if localBroker:
+        localBrokerHost = cfg.get('local','name')
+        localBrokerPort = cfg.get('local','port')
+
+    print( localBrokerHost )
+    print( localBrokerPort )
+
+#    rc = redis.StrictRedis(host=localBrokerHost, port=localBrokerPort, db=0)
+    rc = redis.StrictRedis(localBrokerHost, port=6379, db=0)
+
+    for k in rc.scan_iter("/*"):
+        print(str(k))
+#        print(str(k), rc.get(k))
+
+    return
+
+main()
+
