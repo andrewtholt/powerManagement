2271350b4f271297f6074a05963aedc1ac0ecb5a
Andrew Holt <andrewtholt60@gmail.com>
2017-04-10 13:30:50 +0100

    Expanded options.

--- PDU/dPower.py
+++ PDU/dPower.py
@@ -34,8 +34,12 @@ def main():
     target="*"
     operation="STATUS"
 
+    location="home"
+    zone="*"
+    target="*"
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "c:ht:o:v", ["config=","help","target=","operation=","verbose"])
+        opts, args = getopt.getopt(sys.argv[1:], "c:ht:o:vz:", ["config=","help","target=","operation=","verbose","zone="])
 
         for o,a in opts:
             if o in ["-h","--help"]:
@@ -49,16 +53,21 @@ def main():
                 operation=a
             elif o in ["-v","--verbose"]:
                 verbose=True
+            elif o in ["-z","--zone="]:
+                zone=a
 
 
     except getopt.GetoptError as err:
         print(err)
         sys.exit(2)
 
+    base="/" + location + "/" + zone + "/" + target + "/*"
+
     if verbose:
         print("Config   : " + configFile)
         print("Target   : " + target )
         print("Operation:" + operation)
+        print("Base     :" + base)
 
     cfg = cp.ConfigParser()
     cfg.read( configFile )
@@ -85,7 +94,7 @@ def main():
 
     if request == "STATUS":
         print("Status")
-        topics=rc.keys( "*/" + out + "/power" )
+        topics=rc.keys( base )
 
         for t in topics:
             k=t.decode(encoding='UTF-8')
@@ -106,7 +115,8 @@ def main():
 
         localClient.connect(localMqttHost, localMqttPort, 60)
 
-        topics=rc.keys( "*/" + out + "/power" )
+#        topics=rc.keys( "*/" + out + "/power" )
+        topics=rc.keys( base )
         for t in topics:
             k=t.decode(encoding='UTF-8')
             print( k +" " + request )
