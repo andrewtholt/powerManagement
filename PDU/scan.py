97807f88cd8c14eaeb647c683c74fc034c3b7ded
Andrew Holt <andrewtholt60@gmail.com>
2017-05-19 17:51:53 +0100

    Publish to multiple topics, with same message.

--- PDU/scan.py
+++ PDU/scan.py
@@ -2,6 +2,10 @@
 
 import getopt
 import sys
+import paho.mqtt.client as mqtt
+
+from time import sleep
+
 
 def usage():
     print("Usage: scan.py -h|-t <topic> -m <msg>")
@@ -12,21 +16,26 @@ def main():
     msg = ""
     verbose = True
     host = "127.0.0.1"
+    port = 1883
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ht:m:",["help","topic","message"])
+        opts, args = getopt.getopt(sys.argv[1:], "?t:m:p:h:",["help","topic","message","port","host"])
     except getopt.GetoptError as err:
         print(err)
         sys.exit(2)
 
     for o,a in opts:
-        if o in ("-h","--help"):
+        if o in ("-?","--help"):
             usage()
             sys.exit(0)
         elif o in ("-t","--topic"):
             topic = a
         elif o in ("-m","--message"):
             msg = a
+        elif o in ("-h","--host"):
+            host = a
+        elif o in ("-p","--port"):
+            port = int(a)
 
     if topic == "" or msg == "":
         usage()
@@ -35,8 +44,17 @@ def main():
     if verbose:
         print("Topic:" + topic)
         print("Msg  :" + msg)
+        print("Host :" + host)
+        print("Port :", port)
+
+    client = mqtt.Client()
+    client.connect(host,port,60)
 
     for t in topic.split():
         print(t)
+        print(msg)
+
+        client.publish(t,msg,qos=0,retain=True)
+        sleep(0.1)
 
 main()
