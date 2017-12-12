8f78f228a6ea13036a18b991011cde242c6b0464
andrew holt <andrew.holt@elcometer.com>
2017-12-12 09:17:19 +0000

    Start of new power outlet management

---
+++ newPDU/pm8.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+verbose=False
+child=None
+exitFlag=False
+
+outlets={ '/home/office/proliant/power': '3' }
+
+import sys
+import signal
+import time
+
+import pexpect
+import getopt
+import paho.mqtt.client as mqtt
+import configparser as cp
+
+from os import getenv
+
+def handler(signum, frame):
+    global exitFlag
+    print('Signal handler called with signal', signum)
+
+    if signum == 1:
+        print("Exit from pm8")
+        child.sendline('')
+        child.expect('pm>')
+        child.sendline('exit')
+        time.sleep(2)
+        exitFlag=True
+
+
+
+def usage():
+    print()
+    print("Usage:pm8.py -h|-c <cfg> ")
+
+def on_connect(client, userdata, flags, rc):
+    global verbose
+    global exitFlag
+
+    if exitFlag:
+        sys.exit(0)
+
+    if verbose:
+        print("Connected")
+
+    for k,v in outlets.items():
+        print(k)
+        client.subscribe( k )
+
+#    client.subscribe("/home/office/proliant/power")
+
+def on_message(client, userdata, msg):
+    global verbose
+    global outlets
+
+    m=(msg.payload).decode()
+    t=msg.topic
+
+    if verbose:
+        print("Message")
+        print("Topic : " + t )
+        print("Msg   : " + m )
+        print("=======")
+
+
+    out = str(outlets[ t ])
+
+    pm8Switch( out , m )
+
+
+def pm8Switch( out, state ):
+    global verbose
+
+    cmd = state.lower() + ' ' + str(out)
+
+    if verbose:
+        print( cmd )
+    
+    child.sendline( cmd )
+#    child.expect('pm8')
+
+
+
+def pm8Connect( host, port):
+    global child;
+    cmd = 'nc ' + host + ' ' + str(port)
+
+    child = pexpect.spawn(cmd)
+    
+    child.sendline('')
+    
+    i=child.expect(['Username:','pm>'])
+    
+#    print("i=",i)
+    
+    if i == 0:
+        child.sendline('admin')
+        child.expect('Password:')
+        child.sendline('pm8')
+    elif i==1:
+        print("Already logged in")
+    
+#    child.sendline("off 1")
+    
+#    child.expect("Outlet ")
+    child.sendline('')
+    child.expect('pm>')
+
+    print(child.before)
+
+
+
+def main():
+    global verbose
+
+    pm8Host = 'localhost'
+    pm8Port = 4001
+
+    configFile = "/etc/mqtt/bridge.ini"
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "c:hvn:p:", ["config=","help","verbose","host","port"])
+
+        for o,a in opts:
+            if o in ["-h","--help"]:
+                usage()
+                sys.exit()
+            elif o in ["-c","--config"]:
+                configFile = a
+            elif o in ["-v","--verbose"]:
+                verbose=True
+            elif o in ["-n","--host"]:
+                pm8Host = a
+            elif o in ["-p","--port"]:
+                pm8Host = int(a)
+
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+
+    cfg = cp.ConfigParser()
+    cfg.read( configFile )
+
+    mqttBroker=cfg.get('local','name')
+    mqttPort=int(cfg.get('local','port'))
+
+    if verbose:
+        print("MQTT Broker " + mqttBroker)
+        print("MQTT Port   " + str(mqttPort))
+    #   
+    # Connect to mqtt broker.
+    #   
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(mqttBroker, mqttPort, 60) 
+
+#    pm8Connect( 'proliant', 4001)
+    pm8Connect( pm8Host, pm8Port)
+
+    signal.signal(signal.SIGHUP, handler)
+
+    client.loop_forever()
+
+    print("HERE")
+
+    sys.exit(0)
+
+main()
+
---
+++ newPDU/setup.sql
@@ -0,0 +1,78 @@
+drop table if exists hosts;
+drop table if exists outlets;
+drop table if exists ups;
+
+create table hosts 
+( idx integer primary key autoincrement,
+    name varchar(32) not null unique,
+    ip varchar(32),
+    mac varchar(32),
+    --
+    -- type is used by the pdu processing,
+    -- Currently apc and cyc are used.
+    -- Could be used to hold model info.
+    --
+    -- 26 Feb 2017: Continue as above but these are effectively
+    -- aliases for snmp.  Add types: snmp and mqtt
+    --
+    type varchar(8) default 'UNKNOWN',
+    -- snmp port
+    port integer default 161,
+    rw_community varchar(32) default 'private',
+    ro_community varchar(32) default 'public',
+    on_value integer,
+    off_value integer,
+    reboot_value integer,
+    -- 
+    -- end of snmp
+    --
+    status varchar(8) default 'UNKNOWN',
+    ping_count integer default 3,
+    ping_counter integer default -1,
+    touched date default CURRENT_TIMESTAMP
+);
+
+create table outlets
+( idx integer primary key autoincrement,
+    hostidx int not null,
+    name varchar(32) unique,
+    delay integer default 10,
+    -- 
+    -- 26 Feb: if the outlet is 'hosted' by a none
+    -- snmp type, suchas MQTT the oid is reused as (say)
+    -- 'topic'
+    --
+    oid varchar(128),
+    number int,
+    state varchar(8) default 'UNKNOWN',
+    requested_state varchar(8) default 'NA',
+    initial_state varchar(8) default 'OFF',
+    -- start state
+    --
+    -- Valid values: RESTORE, ON, OFF
+    --
+    start_state varchar(8) default 'RESTORE',
+    locked varchar(4) default 'NO',
+    shutdown_cmd varchar(64) default 'NONE',
+    pf_action varchar(64) default 'NONE',
+    pf_state varchar(8) default 'UNKNOWN',
+    touched date default CURRENT_TIMESTAMP,
+    act_when date
+);
+
+create table ups
+(   idx integer primary key autoincrement,
+    name varchar(32) not null unique,
+    charge integer default -1,
+    threshold integer integer default 99,
+    --
+    -- state 
+    --   UNKNOWN
+    --   ONBATT
+    --   ONLINE
+    port integer default 161,
+    state varchar(8) not null default 'UNKNOWN',
+    delay integer default 60,
+    touched date default CURRENT_TIMESTAMP
+);
+
