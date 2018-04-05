10e508b506daa90e3f82089e87ffe3d27ff18dd7
Andrew Holt (On Laptop) <andrewtholt60@gmail.com>
2018-04-05 19:59:58 +0100

    Log activity on an MQTT topic.

---
+++ Automation/logger.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import sys
+import getopt
+import os.path
+from os import getenv
+import paho.mqtt.client as mqtt
+import configparser as cp
+import datetime
+
+sys.path.append(".")
+
+import logicVM as logic
+
+
+verbose=False
+machine=None
+
+subCount=0
+
+def usage():
+    print("Usage: logic.py ...")
+
+
+def on_message(client, userdata, msg):
+    global subCount
+
+    print("On Message")
+    state = (msg.payload).decode("utf-8")
+
+    now = datetime.datetime.now()
+    print( now, end="" )
+    print(">>> " + msg.topic+" "+ state )
+
+
+
+def on_connect(client, userdata, flags, rc):
+    print("On connect")
+
+def main():
+    global verbose
+    global db
+    global subCount
+    global machine
+
+    configFile="/etc/mqtt/bridge.ini"
+
+    print( sys.argv[1] )
+
+    if os.path.exists(configFile):
+        cfg = cp.ConfigParser()
+        cfg.read( configFile )
+        mqttBroker=cfg.get('local','name')
+        mqttPort=int(cfg.get('local','port'))
+    else:
+        print('No such file as ' + configFile, file=sys.stderr)
+        sys.exit(2)
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(mqttBroker, mqttPort, 60)
+
+    client.subscribe( sys.argv[1] )
+
+    client.loop_forever()
+
+
+main()
