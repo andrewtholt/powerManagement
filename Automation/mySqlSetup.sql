c717cc981a9160bd42aefa77c62eaae82108351d
Andrew Holt (On Laptop) <andrewtholt60@gmail.com>
2018-06-03 15:52:01 +0100

    Added INTERNAL as a type.

--- Automation/mySqlSetup.sql
+++ Automation/mySqlSetup.sql
@@ -1,4 +1,10 @@
 
 drop table if exists io_point;
 
-create table io_point ( name varchar(32) not null unique, topic varchar(128) default 'NONE', direction enum('IN','OUT','DISABLED','INTERNAL') not null default 'DISABLED', state varchar(8) not null default 'UNKNOWN', on_state  varchar(6) default 'ON', off_state varchar(6) default 'OFF');
+create table io_point ( name varchar(32) not null unique,
+    topic varchar(128) default 'NONE',
+    direction enum('IN','OUT','DISABLED','INTERNAL') not null default 'DISABLED',
+    state varchar(8) not null default 'UNKNOWN',
+    on_state  varchar(6) default 'ON',
+    off_state varchar(6) default 'OFF'
+);
