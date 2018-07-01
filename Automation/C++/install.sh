e323cb9e4709d7c4bac4aabbab843e0cc06aeb41
Andrew Holt (On Laptop) <andrewtholt60@gmail.com>
2018-07-01 19:55:09 +0100

    Check for mysql.

--- Automation/C++/install.sh
+++ Automation/C++/install.sh
@@ -13,9 +13,11 @@ fi
 echo "... done"
 
 echo "Check for mysql ..."
-which mysqld
-if [ $? -ne 0 ]; then
-    echo "Fatal error, no mysqld found"
+SERVER=$(dpkg -l | grep mysql-server | wc -l)
+
+if [ $SERVER -eq 0 ]; then
+    echo "Fatal error, no mysqld found."
+    echo "Please install."
     exit 1
 fi
 echo "... done"
