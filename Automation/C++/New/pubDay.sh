48dae563148b633630b96590437da4f97cbb3d57
Andrew Holt(onRPi0) <andrewtholt60@gmail.com>
2018-12-01 11:52:09 +0000

    Added in pass topic as command line param.

--- Automation/C++/New/pubDay.sh
+++ Automation/C++/New/pubDay.sh
@@ -2,7 +2,13 @@
 
 export PATH="/home/andrewh/bin:/home/andrewh/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
 
-mosquitto_pub -t "/test/day" -m $(date "+%j") -h 192.168.0.65 -r
+TOPIC="/test/day"
+
+if [ $# -gt 0 ]; then
+    TOPIC=$1
+fi
+
+mosquitto_pub -t $TOPIC -m $(date "+%j") -h 192.168.0.65 -r
 
 
 
--- Automation/C++/New/pubTime.sh
+++ Automation/C++/New/pubTime.sh
@@ -2,7 +2,13 @@
 
 export PATH="/home/andrewh/bin:/home/andrewh/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
 
-mosquitto_pub -t "/test/time" -m $(date "+%H:%M") -h 192.168.0.65 -r
+TOPIC="/test/time"
+
+if [ $# -gt 0 ]; then
+    TOPIC=$1
+fi
+
+mosquitto_pub -t $TOPIC -m $(date "+%H:%M") -h 192.168.0.65 -r
 
 
 
