414383767cf60bda0f54421c8fdbb348a057157c
andrewtholt <andrewtholt60@gmail.com>
2018-12-01 13:19:17 +0000

    Simple scripts to publish time and day of year.

---
+++ Automation/C++/New/pubDay.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PATH="/home/andrewh/bin:/home/andrewh/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
+
+mosquitto_pub -t "/test/day" -m $(date "+%j") -h 192.168.0.65
+
+
+
+
