cd7c80b28e28d33579125934234e444c337b1ba4
andrewtholt <andrewtholt60@gmail.com>
2018-12-15 15:41:31 +0000

    Shut off debug and print usefull messages.

--- Automation/C++/New/install.sh
+++ Automation/C++/New/install.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-set -x
+# set -x
 
 RELEASE="0.1"
 LIB_DEST="/usr/local/lib"
 INC_DEST="/usr/local/include"
 
+echo "Copy libs"
 sudo cp ./libplc.so $LIB_DEST
 HERE=$(pwd)
 cd $LIB_DEST
 
 if [ -L ./libplc.so.${RELEASE} ]; then
     echo "./libplc.so.${RELEASE} already exists"
-    exit 1
 else
+    echo "Make symbolic link"
     sudo ln -s ./libplc.so ./libplc.so.${RELEASE}
 fi
 
 cd $HERE
+
+echo "copy headers"
+
 sudo cp plcMQTT.h $INC_DEST
 sudo cp plcBase.h $INC_DEST
 
