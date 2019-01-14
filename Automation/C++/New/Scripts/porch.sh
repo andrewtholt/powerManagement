dbc2b6502c486fa53f13e1b943a8ad908b340cde
andrewh <andrewtholt60@gmail.com>
2019-01-14 13:39:25 +0000

    Mover to own repo https://github.com/andrewtholt/PLC.git

--- Automation/C++/New/Scripts/porch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-
-curl -v -X GET \
-    -H "Content-Type: application/json" \
-    http://192.168.0.65:8123/api/states/switch.porch_light
--- Automation/C++/New/Scripts/sun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-
-curl -X GET \
-    -H "Content-Type: application/json" \
-    http://192.168.0.65:8123/api/states/sun.sun
