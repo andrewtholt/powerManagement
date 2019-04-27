0728e5fd99a5f1ecba75d9ccc7f875ef759b7acf
andrewtholt <andrewtholt60@gmail.com>
2019-04-27 09:43:46 +0100

    Add autoncrement index to each table and current states UNTESTED.

--- Automation/mysqlNewSetup.sql
+++ Automation/mysqlNewSetup.sql
@@ -3,6 +3,7 @@ drop table if exists io_type;
 drop table if exists mqtt;
 drop table if exists modbus;
 drop table if exists snmp;
+drop table if exists internal;
 -- 
 CREATE TABLE IF NOT EXISTS io_type (
     ioType VARCHAR(32) NOT NULL UNIQUE
@@ -16,7 +17,6 @@ insert into io_type (ioType) values ( 'MQTT');
 insert into io_type (ioType) values ( 'MODBUS');
 insert into io_type (ioType) values ( 'SNMP');
 
-
 CREATE TABLE IF NOT EXISTS io_point (
     name VARCHAR(32) NOT NULL UNIQUE,
     direction ENUM('IN', 'OUT', 'DISABLED', 'INTERNAL') NOT NULL DEFAULT 'DISABLED',
@@ -24,23 +24,36 @@ CREATE TABLE IF NOT EXISTS io_point (
 );
 
 
+CREATE TABLE IF NOT EXISTS internal (
+    idx int IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(32) NOT NULL UNIQUE,
+    on_state VARCHAR(32) NOT NULL DEFAULT 'TRUE',
+    off_state VARCHAR(32) NOT NULL DEFAULT 'FALSE'
+    state VARCHAR(32) NOT NULL DEFAULT 'FALSE'
+    );
+
 CREATE TABLE IF NOT EXISTS mqtt (
+    idx int IDENTITY(1,1) PRIMARY KEY,
     name VARCHAR(32) NOT NULL UNIQUE,
     topic VARCHAR(32) NOT NULL UNIQUE,
     on_state VARCHAR(32) NOT NULL DEFAULT 'TRUE',
     off_state VARCHAR(32) NOT NULL DEFAULT 'FALSE'
+    state VARCHAR(32) NOT NULL DEFAULT 'FALSE'
 );
 
 create table if not exists  snmp ( 
+    idx int IDENTITY(1,1) PRIMARY KEY,
     name varchar(32) not null unique,
     oid varchar(255) not null unique,
     ipAddress varchar(16) not null,
     port int not null,
     on_state varchar(32) not null default 'TRUE',
     off_state varchar(32) not null default 'FALSE'
+    state VARCHAR(32) NOT NULL DEFAULT 'FALSE'
 );
 
 CREATE TABLE IF NOT EXISTS modbus (
+    idx int IDENTITY(1,1) PRIMARY KEY,
     name VARCHAR(32) NOT NULL UNIQUE,
     address INT NOT NULL UNIQUE,
     functionCode INT NOT NULL,
@@ -48,4 +61,5 @@ CREATE TABLE IF NOT EXISTS modbus (
     port INT NOT NULL,
     on_state VARCHAR(32) NOT NULL DEFAULT 'TRUE',
     off_state VARCHAR(32) NOT NULL DEFAULT 'FALSE'
+    state VARCHAR(32) NOT NULL DEFAULT 'FALSE'
 );
