171e922c98d49a66d857d380843beedf7dec673b
andrewtholt <andrewtholt60@gmail.com>
2019-04-27 15:56:19 +0100

    removed redundant name from mqtt etc and fixed tstData.

--- Automation/tstData.sql
+++ Automation/tstData.sql
@@ -3,12 +3,12 @@
 -- delete from mqtt;
 
 replace into mqtt (name,topic ) values ('START','/test/start');
-replace into io_point (name,direction ) values ('START','IN');
+replace into io_point (direction ) values ('IN');
 
-replace into mqtt (name,topic ) values ('STOP','/test/stop');
+replace into mqtt (topic ) values ('/test/stop');
 replace into io_point (name,direction ) values ('STOP','IN');
 
-replace into mqtt (name,topic ) values ('MOTOR','/test/motor');
+replace into mqtt (topic ) values ('/test/motor');
 replace into io_point (name,direction ) values ('MOTOR','OUT');
 
 update io_point,mqtt set io_point.io_idx=mqtt.idx where io_point.name = mqtt.name;
