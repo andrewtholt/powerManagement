f474ab0b8a5bd24d68a515283dda5f4e22b1de7f
andrewh <andrewtholt60@gmail.com>
2019-04-30 15:39:51 +0100

    More testing with xml

---
+++ usingJSON/getMonit1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import urllib.parse
+import urllib.request
+
+import xml.etree.ElementTree as ET
+
+password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+
+top_level_url = 'http://127.0.0.1:2812/'
+
+username="admin"
+password="monit"
+
+password_mgr.add_password("monit", top_level_url, username, password)
+handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+opener = urllib.request.build_opener(handler)
+
+
+response = opener.open('http://127.0.0.1:2812/_status?format=xml')
+xmlData = response.read()
+
+root = ET.fromstring(xmlData)
+
+tst = root[2]
+# tst = root
+
+print(tst[0].text)
+
+count = 0
+for i in tst:
+    print("count ",count)
+
+    print(i.tag , "=" , i.text )
+    count = count + 1
+    print("=====")
+
