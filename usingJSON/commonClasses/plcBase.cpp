f43ab06b31857fc453fda44397011d890179048b
andrewtholt <andrewtholt60@gmail.com>
2019-05-15 17:59:34 +0100

    Started on really base plc

---
+++ usingJSON/commonClasses/plcBase.cpp
@@ -0,0 +1,283 @@
+/***********************************************************************
+ * AUTHOR: Andrew Holt <andrewh>
+ *   FILE: .//plcBase.cpp
+ *   DATE: Thu Mar  7 18:19:45 2019
+ *  DESCR: 
+ ***********************************************************************/
+#include "plcBase.h"
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <cstring>
+#include <string>
+
+/*
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+*/
+
+using namespace std;
+
+void plcBase::setVerbose(bool flag) {
+	verbose = flag;
+}
+/***********************************************************************
+ *  Method: plcBase::getValue
+ *  Params: string shortName
+ * Returns: string
+ * Effects: 
+ ***********************************************************************/
+string plcBase::getValue(string shortName) {
+//    json rec = getRow(shortName);
+
+//    return(rec["msg"]);
+    return("UNIMPLEMENTED");
+}
+
+
+/***********************************************************************
+ *  Method: plcBase::setValue
+ *  Params: string shortName, string v
+ * Returns: bool
+ * Effects: 
+ ***********************************************************************/
+void plcBase::setValue(string shortName, string v) {
+    /*
+    json outMsg=getRow( shortName) ;
+
+    outMsg["msgType"]  = "DATA";
+    outMsg["cmd"]      = "SET";
+    outMsg["shortName"]=shortName;
+    outMsg["direction"]="OUT";
+    outMsg["msg"]=v;
+
+    string out = outMsg.dump();
+    write(fd, out.c_str(), out.length());
+    */
+}
+
+/***********************************************************************
+ *  Method: plcBase::getOldValue
+ *  Params: string
+ * Returns: string
+ * Effects: 
+ ***********************************************************************/
+string plcBase::getOldValue(string) {
+    string ov="";
+
+    return ov;
+}
+
+
+/***********************************************************************
+ *  Method: plcBase::setOldValue
+ *  Params: string shortName, string v
+ * Returns: bool
+ * Effects: 
+ ***********************************************************************/
+bool plcBase::setOldValue(string shortName, string v) {
+    bool failFlag=true;
+
+    return failFlag;
+}
+
+
+/***********************************************************************
+ *  Method: plcBase::getBoolValue
+ *  Params: string
+ * Returns: bool
+ * Effects: 
+ ***********************************************************************/
+bool plcBase::getBoolValue(string name) {
+    bool value=false;
+
+    string v = getValue(name);
+
+    if( v == "UNKNOWN") {
+        v= false;
+    }
+
+    if( v == "UNDEFINED") {
+        v= false;
+    }
+
+    if( v == "NODATA") {
+        v= false;
+    }
+
+    value = (v=="ON") ? true : false ;
+
+    return value;
+}
+
+
+/***********************************************************************
+ *  Method: plcBase::setBoolValue
+ *  Params: string shortName, bool v
+ * Returns: bool
+ * Effects: 
+ ***********************************************************************/
+bool plcBase::setBoolValue(string shortName, bool v) {
+    bool failFlag=true;
+
+    return failFlag;
+}
+
+
+/***********************************************************************
+ *  Method: plcBase::getBoolOldValue
+ *  Params: string
+ * Returns: bool
+ * Effects: 
+ ***********************************************************************/
+bool plcBase::getBoolOldValue(string) {
+    bool failFlag=true;
+
+    return failFlag;
+}
+
+
+/***********************************************************************
+ *  Method: plcBase::setBoolOldValue
+ *  Params: string shortName, bool v
+ * Returns: bool
+ * Effects: 
+ ***********************************************************************/
+
+bool plcBase::setBoolOldValue(string shortName, bool v) {
+    bool failFlag=true;
+
+    return failFlag;
+}
+
+
+/***********************************************************************
+ *  Method: plcBase::plcBase
+ *  Params: 
+ * Effects: 
+ ***********************************************************************/
+plcBase::plcBase(int f) {
+
+    fd = f;
+}
+
+
+/***********************************************************************
+ *  Method: plcBase::~plcBase
+ *  Params: 
+ * Effects: 
+ ***********************************************************************/
+plcBase::~plcBase()
+{
+}
+//
+// return the value TOS and
+// remove it from the stack.
+//
+bool plcBase::Pop() {
+    bool a = logicStack.top();
+    logicStack.pop();
+
+    return a;
+}
+
+
+void plcBase::Ld(string symbol) {
+    try {
+        cacheHit = true;
+        if(verbose) {
+            cout << "plcBase::Ld " << symbol ;
+        }
+
+        bool v=getBoolValue( symbol );
+        logicStack.push( v );
+
+        if(verbose) {
+            cout << "   TOS: " << logicStack.top() << endl;
+        }
+    } catch(int e) {
+        cerr << symbol + ":Undefined " << e << endl;
+    }
+}
+
+void plcBase::And() {
+    if(verbose) {
+        cout << "plcBase::And " ;
+    }
+
+    bool a = Pop()  ;
+    bool b = Pop()  ;
+
+    bool v = a&b;
+    logicStack.push(v);
+
+    if(verbose) {
+        cout << "   TOS: " << logicStack.top() << endl;
+    }
+}
+
+void plcBase::Or() {
+    if(verbose) {
+        cout << "plcBase::Or " ;
+    }
+
+    bool a = Pop()  ;
+    bool b = Pop()  ;
+
+    bool v = a|b;
+    logicStack.push(v);
+
+    if(verbose) {
+        cout << "   TOS: " << logicStack.top() << endl;
+    }
+}
+
+void plcBase::Invert() {
+    if(verbose) {
+        cout << "plcBase::Not " ;
+    }
+
+    bool a = Pop()  ;
+
+    bool v = !a;
+    logicStack.push(v);
+
+    if(verbose) {
+        cout << "   TOS: " << logicStack.top() << endl;
+    }
+}
+
+void plcBase::Out(string symbol) {
+// Overload
+}
+
+/***********************************************************************
+ *  Method: plcBase::End
+ *  Params: int delay
+ * Returns: void
+ * Effects: 
+ ***********************************************************************/
+void plcBase::End(int delay) {
+
+}
+
+
+
+#ifdef __cplusplus
+extern "C" {
+    /*
+       struct plcBase *newPlcBase(spreadBase *spr) {
+       struct plcBase *ptr = new plcBase(spr);
+
+       return ptr ;
+       }
+     */
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
---
+++ usingJSON/commonClasses/plcBase.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <map>
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+#ifdef __cplusplus
+extern "C" {
+// struct plcBase *newPlcBase(spreadBase *spr);
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+using namespace std;
+
+class plcBase {
+
+    private:
+		bool verbose=false;
+		stack<bool> logicStack;
+
+		int fd;
+		map<string, string> ioPoint;   // name, value
+
+		bool cacheHit = true;
+
+        string getValue( string shortName);
+        string getOldValue( string );
+        void setValue(string shortName, string v );
+        bool setOldValue(string shortName, string v );
+        // 
+        // Syntactic sugar.
+        //
+        bool getBoolValue( string );
+        bool setBoolValue(string shortName, bool v );
+        bool getBoolOldValue( string );
+        bool setBoolOldValue(string shortName, bool v );
+
+
+	public:
+		plcBase(int fd);
+		~plcBase();
+    	void cacheDump();
+
+    	void setVerbose(bool );
+
+		bool Pop();     // Remove and return top stack entry
+        void Push(bool v); // Push to stack.
+		void Ld(string symbol);
+		void Or();
+		void And();
+		void Invert();
+
+		void Out(string symbol);
+		void End(int delay);
+
+};
