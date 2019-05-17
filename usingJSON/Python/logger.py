56f662b6f27e49a0897ad00b90962d82bd43d636
andrewh <andrewtholt60@gmail.com>
2019-05-17 09:33:45 +0100

    Logs as epoc time.

--- usingJSON/Python/logger.py
+++ usingJSON/Python/logger.py
@@ -1,56 +1,123 @@
 #!/usr/bin/env python3
-from time import gmtime, strftime
+
+import sys 
+import os.path
+import pymysql as sql 
+import json
 import paho.mqtt.client as mqtt
-import sqlite3
+# import time
+from time import gmtime, strftime, time
+import getopt
 
-# start_topic = "/home/#"
+db = None
+
+connected=False
 start_topic = "/test/#"
-
 data = {}
+verbose = True
 
-def init():
+logFd = None
+
+def stateToLogic( state ) :
+
+    if state in ["ON","TRUE"]:
+        return "TRUE"
+
+    if state in ["OFF","FALSE"]:
+        return "FALSE"
+
+def usage():
+    print("Usage: logger.py -h|-l <file>| -c <cfg file>")
+
+def on_message(client, userData,msg):
     global data
-
-    data["START"] = "OFF"
-
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe(start_topic)
+    global logFd
     
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    global data
     shortName = ""
 
     theTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    aTime = str(int(time()))
 
     result = (msg.payload).decode("utf-8")
-
-    if (msg.topic == start_topic):
-        shortName = "START"
     # 
     # Update storage here
     # 
-    print(theTime,shortName + " : ",msg.topic, result);
+    if logFd == None:
+#        print(theTime,shortName + " : ",msg.topic, result);
+        print(aTime,shortName + " : ",msg.topic, result);
+    else:
+#        logFd.write(theTime + ":")
+        logFd.write(aTime + ":")
+        logFd.write(msg.topic + " ")
+        logFd.write(result )
+        
+        logFd.write("\r\n")
+        logFd.flush()
     return
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    global connected
+    connected=True
+    #
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    #
+    client.subscribe(start_topic)
 
-init()
+def main():
+#    global db
+    global logFd
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+    configFile="/etc/mqtt/bridge.json"
+    logFile = None
 
-# client.connect("raspberrypi", 1883, 60)
-client.connect("localhost", 1883, 60)
-# client.connect("192.168.0.65", 1883, 60)
+    try:
+        opts,args = getopt.getopt(sys.argv[1:], "c:hl:", ["config=","help","logfile="])
+        for o,a in opts:
+            if o in ["-h","--help"]:
+                usage()
+                sys.exit()
+            elif o in ["-c","--config"]:
+                configFile = a
+            elif o in ["-l","--logfile"]:
+                logFile = a
+                print(logFile)
+                
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+    mqttBroker = 'localhost'
+    mqttPort = 1883
+
+    if os.path.exists(configFile):
+        with open( configFile, 'r') as f:
+            cfg = json.load(f)
+
+        mqttBroker = cfg['local']['name']
+        mqttPort   = int(cfg['local']['port'])
+    else:
+        print(configFile + " not found..")
+        print(".. using defaults")
+
+    if verbose:
+        print("MQTT Broker : " + mqttBroker)
+        print("       Port : " , mqttPort)
+        print("      Topic : " , start_topic)
+        print("      Log   : " , logFile)
+
+    if logFile != None:
+        logFd = open(logFile,"w+")
+        
+    mqttClient = mqtt.Client()
+    mqttClient.on_connect = on_connect
+    mqttClient.on_message = on_message
+
+    mqttClient.connect(mqttBroker, mqttPort, 60)
+
+    global connected
+    mqttClient.loop_forever()
+
+main()
