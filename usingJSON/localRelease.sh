52fba435205af15021493b1cbd60ffd0e33294ea
andrewh <andrewtholt60@gmail.com>
2019-05-24 09:49:53 +0100

    More on localRelease.sh

--- usingJSON/localRelease.sh
+++ usingJSON/localRelease.sh
@@ -4,5 +4,34 @@ set -x
 
 TARGET=./output
 if [ ! -d $TARGET ]; then
-    mkdir -p $TARGET
+    mkdir -p $TARGET/
 fi
+
+mkdir -p $TARGET/bin
+mkdir -p $TARGET/etc
+
+if [ ! -d ./Debug ]; then
+    echo "Nothing to install"
+    exit 1
+fi
+
+BINS="dispatch Server"
+
+for b in $BINS; do
+    echo $b
+    cp ./Debug/src/${b} $TARGET/bin
+done
+
+PYTHON="syncMQTT.py"
+
+for p in $PYTHON; do
+    echo $p
+    cp ./Python/${p} $TARGET/bin
+done
+
+CFG="bridge.json"
+
+for c in $CFG; do
+    echo $c
+    cp ./JSON/${c} $TARGET/etc
+done
