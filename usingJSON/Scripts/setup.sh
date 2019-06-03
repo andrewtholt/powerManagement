a53092bf18b778a50059926496f8a2546f5752bd
andrewh <andrewtholt60@gmail.com>
2019-06-03 09:53:34 +0100

    Setup:create user and message Q

---
+++ usingJSON/Scripts/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# set -x
+# 
+# Check if user 'power' exists, if not create.
+#
+USER="power"
+QUEUE="toDispatcher"
+
+getent passwd $USER > /dev/null 2&>1
+
+if [ $? -eq 0  ]; then
+    echo "User $USER exists"
+else
+    echo "User $USER does not exist, create ..."
+    sudo useradd --system power
+    echo "... done."
+fi
+
+sudo usermod -s /bin/false power
+
+if [ -f "/dev/mqueue/${QUEUE}" ]; then
+    echo "Message Q ${QUEUE} exists."
+else
+    echo "Message Q ${QUEUE} does not exist, creating ..."
+    mqMake -q $QUEUE
+    echo "... done."
+fi
+
+echo "Fixing ${QUEUE} properties ..."
+sudo chown power:power /dev/mqueue/toDispatcher
+sudo chmod 660 /dev/mqueue/toDispatcher
+echo "... done."
+
