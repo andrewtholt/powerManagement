f218ca72d3b078c359b39d73293c348b6f392a6a
Andrew Holt (rpi3) <andrewtholt60@gmail.com>
2019-06-15 14:07:42 +0100

    Added command line options to set time and state of inputs.

--- usingJSON/commonClasses/tstAlarm.cpp
+++ usingJSON/commonClasses/tstAlarm.cpp
@@ -4,35 +4,94 @@
 
 using namespace std;
 
-int main() {
+void usage() {
+    cout << "Help" << endl;
+}
+
+int main(int argc, char *argv[]) {
 
     string alarmTime;
     int duration=0;
-    string endTime;
+    
+    bool verbose;
+    bool invert=false;
+
+    string startTime="";
+    string endTime="";
+
+    int opt;
+
+    while((opt = getopt(argc, argv, "hvis:e:")) != -1) {
+        switch( opt ) {
+            case 'h':
+                usage();
+                exit(1);
+                break;
+            case 'v':
+                verbose=false;
+                break;
+            case 's':
+                startTime=optarg;
+                break;
+            case 'e':
+                endTime=optarg;
+                break;
+            case 'i':
+                invert=true;
+                break;
+        }
+    }
+
+    if( (startTime == "") || (endTime == "")) {
+        cerr << "Need start and end times." << endl;
+        usage();
+        exit(1);
+    }
 
     alarmBase *t = new alarmBase();
 
     cout << "Unset " << t->checkTime() << endl;;
 
-    alarmTime="13:10";
-    t->setStartTime(alarmTime);
+//    alarmTime="13:10";
+    t->setStartTime(startTime);
 
-    cout << "Set time " + alarmTime + "\t" << t->checkTime() << endl;;
+    cout << "Set time " + startTime + "\t" << t->checkTime() << endl;;
 
+    /*
     duration=10;
     t->setDuration(duration);
     cout << "Set duration " + to_string(duration) + "\t" << t->checkTime() << endl;;
+    */
 
-    endTime = "13:30";
+    endTime = endTime ;
     t->setEndTime(endTime);
 
-    t->dump();
     cout << "Set end time " + endTime + "\t" << t->checkTime() << endl;;
 
-    t->setDow(TUESDAY);
+    t->setDow(ALL);
 
     t->dump();
     cout << "Set dow " + endTime + "\t" << t->checkTime() << endl;;
+
+    int count=0;
+    bool state=false;
+
+    while( true ) {
+
+        cout << endl << "=================" << endl;
+        count++;
+        cout << "Count      " << count << endl;
+        cout << "Start time " + startTime + "\t" << endl;;
+        cout << "End   time " + endTime + "\t" << endl;
+
+        state = t->checkTime() ;
+        if(invert) {
+            state = (state == true) ? false : true;
+        }
+        cout << "\tState " << state << endl;
+
+        sleep(15);
+    }
 
 }
 
