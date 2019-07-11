c858700a62566fb23f7b66cb8801b7b91323d292
Andrew Holt (rpi3) <andrewtholt60@gmail.com>
2019-07-11 11:30:07 +0100

    Added a number of script to pubish the time (hh:mm) and the day of the year, plus ping a list of ip addresses and gave a basic UP/DOWN notification.

---
+++ usingJSON/Scripts/pubDay.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# set -x
+
+export PATH="/home/andrewh/bin:/home/andrewh/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
+
+MQTT=$(cat /etc/mqtt/bridge.json | jq -r .local.name )
+TOPIC=$(cat /etc/mqtt/bridge.json | jq -r .topics.day)
+
+if [ $# -gt 0 ]; then
+    TOPIC=$1
+fi
+
+mosquitto_pub -t $TOPIC -m $(date "+%j") -h $MQTT
+
+
+
+
