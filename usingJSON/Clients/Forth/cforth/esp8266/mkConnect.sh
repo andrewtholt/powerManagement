e1f75e9f6d26b669ccd05d407ed1bdb20dcbde71
andrewtholt <andrewtholt60@gmail.com>
2019-08-14 12:27:24 +0100

    First stab at an 8266 client.

---
+++ usingJSON/Clients/Forth/cforth/esp8266/mkConnect.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# set -x
+
+if [ $# -ne 2 ]; then
+    echo "Usage:./mkConnect <ssid> <password>"
+    echo "And redirect the output to a file"
+    exit 1
+fi
+
+printf "\" %s\" \" %s\" station-connect \n" $1 $2
+printf "ipaddr@ .ipaddr\n"
